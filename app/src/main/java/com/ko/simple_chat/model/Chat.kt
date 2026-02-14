c5c9446170d93636473c137e4fa488c30d678311
kotaegwon <rhxornjs@gmail.com>
2026-02-15 05:20:01 +0900

    model 주석 추가

--- app/src/main/java/com/ko/simple_chat/model/Chat.kt
+++ app/src/main/java/com/ko/simple_chat/model/Chat.kt
@@ -1,5 +1,16 @@
 package com.ko.simple_chat.model
 
+/**
+ * Firebase에서 사용하는 채팅 데이터 클래스
+ *
+ * @param myUid 내 uid
+ * @param otherUid 상대방 uid
+ * @param name 상대방 이름
+ * @param message 메시지
+ * @param time 메시지 전송 시간
+ * @param read 메시지 읽음 여부
+ *
+ */
 data class Chat(
     val myUid: String = "",
     val otherUid: String = "",
--- app/src/main/java/com/ko/simple_chat/model/User.kt
+++ app/src/main/java/com/ko/simple_chat/model/User.kt
@@ -4,6 +4,14 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 
+/**
+ * Firebase에서 사용하는 사용자 데이터 클래스
+ *
+ * @param uid 사용자 uid
+ * @param email 사용자 이메일
+ * @param name 사용자 이름
+ * @param createAt 사용자 생성 시간
+ */
 @Parcelize
 data class User(
     val uid: String = "",
