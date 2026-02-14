2a5fa0d84a7a8e4808dd23e3676ac9dcaa4ae023
kotaegwon <rhxornjs@gmail.com>
2026-02-15 08:18:32 +0900

    툴바 뒤로가기, 메뉴 기능 추가

--- app/src/main/java/com/ko/simple_chat/viewmodel/ChatViewModel.kt
+++ app/src/main/java/com/ko/simple_chat/viewmodel/ChatViewModel.kt
@@ -6,12 +6,23 @@ import androidx.lifecycle.ViewModel
 import com.ko.simple_chat.firebase.FirebaseManager
 import com.ko.simple_chat.model.Chat
 
+/**
+ * 채팅 ViewModel
+ * Firebase에서 채팅 메시지 가져오기
+ * 송신 메시지와 수신 메시지 구분하기 위해 사용
+ */
 class ChatViewModel : ViewModel() {
 
     private val _chatList = MutableLiveData<List<Chat>>()
     val chatList: LiveData<List<Chat>> = _chatList
 
 
+    /**
+     * 채팅 메시지 가져오기
+     *
+     * @param roomId 방 ID
+     * @param onResult 완료 콜백(메시지 목록))
+     */
     fun listenChat(roomId: String) {
         FirebaseManager.listenMessage(roomId) { chatList ->
             _chatList.postValue(chatList)
